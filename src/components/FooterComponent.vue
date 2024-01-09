06375be830e012dfec73a0db58300c4c6002872b
Bhavin Prajapati - TatvaSoft <bhavin.prajapati@tatvasoft.com>
2024-01-09 14:30:23 +0530

    all file removed

--- src/components/FooterComponent.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="footer">
-    <p>V 1.1.0 | Copyright 2023. All rights reserved</p>
-  </div>
-</template>
-<script lang="ts">
-import { Vue } from "vue-class-component";
-export default class FooterComponent extends Vue {}
-</script>
